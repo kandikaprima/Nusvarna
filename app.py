cff8cad088d8e127351871d643aeab446d757a63
Muhammad Al fahri <94788787+alfahri18@users.noreply.github.com>
2023-06-16 02:45:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
-from flask import Flask, render_template, request, redirect, session, jsonify
+from flask import Flask, request, session, jsonify
 import pyrebase
 import cv2
 import numpy as np
 import tensorflow as tf
+from werkzeug.utils import secure_filename
+import os
 
-
-
-app = Flask(__name__, template_folder='template')
+app = Flask(__name__)
 config = {
-  'apiKey': "",
-  'authDomain': "",
-  'databaseURL': "",
-  'projectId': "",
-  'storageBucket': "",
-  'messagingSenderId': "",
-  'appId': "",
-  'databaseURL':''
+  'apiKey': "AIzaSyAa667qOWokjNtRSTP0kTg0ZvTDalQopAI",
+  'authDomain': "moonlit-poetry-381319.firebaseapp.com",
+  'projectId': "moonlit-poetry-381319",
+  'storageBucket': "moonlit-poetry-381319.appspot.com",
+  'messagingSenderId': "167385462735",
+  'appId': "1:167385462735:web:a53855ed0b1a32a65d04ba",
+  'databaseURL':'https://moonlit-poetry-381319-default-rtdb.asia-southeast1.firebasedatabase.app/'
 }
 
 firebase = pyrebase.initialize_app(config)
@@ -23,7 +22,6 @@ auth = firebase.auth()
 db = firebase.database()
 storage = firebase.storage()
 model = tf.keras.models.load_model('current_model.h5')
-class_labels = ['bingka ambon', 'pisang ijo ']
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -37,6 +35,7 @@ def register():
                 'name': nama,
                 'username': username,
                 'email': email,
+                'profile_photo_url': 'https://firebasestorage.googleapis.com/v0/b/moonlit-poetry-381319.appspot.com/o/profile%2FTak%20berjudul115_20230615225802.png?alt=media&token=4ad22332-298d-4114-8737-793b3275dbd2',
                 'poin': 0
             }
             db.child('data_user').child(user['localId']).set(data)         
@@ -46,143 +45,130 @@ def register():
             print(str(e))
             return jsonify({'error': 'Registrasi gagal'}), 400
 
-
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-        try:
-            user = auth.sign_in_with_email_and_password(email, password)
-            token = user['idToken']
-
-            return jsonify({'access_token': token})
-        except:
-            return jsonify({'error': 'password email salah'}), 401
-    return render_template('login.html')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = auth.sign_in_with_email_and_password(email, password)
+        token = user['idToken']
+        return jsonify({'access_token': token})
+    except:
+        return jsonify({'error': 'password email salah'}), 401
 
 @app.route('/profile', methods=['GET'])
 def profile():
     access_token = request.headers.get('Authorization')
     if not access_token:
-        return jsonify({'error': 'Access token tidak ditemukan'}), 401
-
+        return jsonify({'error': 'Access token tidak ditemukan'}), 400
     try:
-        # Verify access token and get user UID
         user = auth.get_account_info(access_token)
         uid = user['users'][0]['localId']
-        # uid = user['uid']
-
-        # Get user data from Realtime Database based on UID
-        user_data = db.child('users').child(uid).get().val()
-
+        user_data = db.child('data_user').child(uid).get().val()
         if not user_data:
-            return jsonify({'error': 'Data pengguna tidak ditemukan'}), 404
-
-        # Send response with user data
+            return jsonify({'error': 'Data pengguna tidak ditemukan'}, uid), 404
         return jsonify(user_data)
-
     except Exception as e:
         print(str(e))
         return jsonify({'error': 'Gagal mengambil data pengguna'}), 400
+  
+@app.route('/profile/edit', methods=['POST'])
+def edit_profile():
+    access_token = request.headers.get('Authorization')
+    if not access_token:
+        return jsonify({'error': 'Access token not found'}), 401
+    try:
+        user = auth.get_account_info(access_token)
+        uid = user['users'][0]['localId']
+        user_data = db.child('data_user').child(uid).get().val()
+        if not user_data:
+            return jsonify({'error': 'User data not found'}), 404
+        new_name = request.form.get('name')
+        if new_name:
+            user_data[1] = new_name
+            db.child('data_user').child(uid).update(user_data)
+            return jsonify({'message': 'Profile updated successfully'})
+        return jsonify({'error': 'No new name provided'}), 400
+    except Exception as e:
+        print(str(e))
+        return jsonify({'error': 'Failed to update profile'}), 400
 
+UPLOAD_FOLDER = '/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/change_password', methods=['POST', 'GET'])
-def change_password():
-        access_token = session.get('access_token')
-        if access_token:
-            try:
-                user = auth.get_account_info(access_token)
-                email = user['users'][0]['email']
-
-                current_password = request.form.get('current_password')
-                new_password = request.form.get('new_password')
-
-                auth.sign_in_with_email_and_password(email, current_password)
-                auth.change_user_password(access_token, new_password) # type: ignore
-
-                return jsonify({'message': 'Password changed successfully'})
-            except:
-                return jsonify({'error': 'Failed to change password'}), 400
-        else:
-            return jsonify({'error': 'User not logged in'}), 401
-
+@app.route('/profile/foto_profile', methods=['POST'])
+def upload_profile_photo():
+    access_token = request.headers.get('Authorization')
+    if not access_token:
+        return jsonify({'error': 'Access token tidak ditemukan', 'keyy' : access_token}), 401
+    try:
+        user = auth.get_account_info(access_token)
+        uid = user['users'][0]['localId']
+        user_data = db.child('data_user').child(uid).get().val()
+        if not user_data:
+            return jsonify({'error': 'Data pengguna tidak ditemukan'}), 404
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file part in the request'}), 400
+        file = request.files['file']
+        if file.filename == '':
+            return jsonify({'error': 'No file selected'}), 400
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename) # type: ignore
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            storage.child('profile').child(filename).put(file_path)
+            download_url = storage.child('profile').child(filename).get_url(None)
+            user_data[3] = download_url 
+            db.child('data_user').child(uid).update(user_data)
+            return jsonify({'message': 'Foto profil berhasil diupload', 'profile_photo_url': download_url})
+        return jsonify({'error': 'File type not allowed'}), 400
+    except Exception as e:
+        print(str(e))
+        return jsonify({'error': 'Gagal mengupload foto profil'}), 500
 
 @app.route('/forgot_password', methods=['POST', 'GET'])
 def forgot_password():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        try:
-            auth.send_password_reset_email(email)
-            return jsonify({'message': 'Password reset email sent'})
-        except:
-            return jsonify({'error': 'Failed to send password reset email'}), 400
-
-    return render_template('forgot_password.html')
+    email = request.form.get('email')
+    try:
+        auth.send_password_reset_email(email)
+        return jsonify({'message': 'Password reset email sent'})
+    except:
+        return jsonify({'error': 'Failed to send password reset email'}), 400
 
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
         if 'image' not in request.files:
             return jsonify({'error': 'No image uploaded'}), 400
-
-        # Read the uploaded image file
         image = request.files['image']
         img = cv2.imdecode(np.frombuffer(image.read(), np.uint8), cv2.IMREAD_COLOR)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-        # Resize the image to 150x150
         img = cv2.resize(img, (150, 150))
-
-        # Preprocess the image
         img = img.astype(np.float32) / 255.0
         img = np.expand_dims(img, axis=0)
-
-        # Make predictions using the model
-        predictions = model.predict(img) # type: ignore
-        predicted_class = np.argmax(predictions)
-        predicted_label = class_labels[predicted_class]
-        if predicted_class == 0:
-            predicted = db.child('baju_tradisional').child('bugis').get().val()
-            return jsonify({'predicted_label': predicted_label}, predicted)
-        elif predicted_class == 1:
-            predicted = db.child('baju_tradisional').child('minang').get().val()
-            return jsonify({'predicted_label': predicted_label}, predicted)
-        elif predicted_class == 2:
-            predicted = db.child('baju_tradisional').child('jawa').get().val()
-            return jsonify({'predicted_label': predicted_label}, predicted)
-        elif predicted_class == 3:
-            predicted = db.child('baju_tradisional').child('papua').get().val()
-            return jsonify({'predicted_label': predicted_label}, predicted)
-        elif predicted_class == 4:
-            predicted = db.child('baju_tradisional').child('kalimantan').get().val()
-            return jsonify({'predicted_label': predicted_label}, predicted)
-        elif predicted_class == 5:
-            predicted = db.child('baju_tradisional').child('bali').get().val()
-            return jsonify({'predicted_label': predicted_label}, predicted)
+        pred = model.predict(img)  # type: ignore
+        ml_class = ["asmat", "bali", "dayak", "madura"]
+        if (pred.max() > 0.5):
+            hasil = np.argmax(pred)
+            predicted = db.child('baju_tradisional').child(ml_class[hasil]).get().val()
+            return jsonify({'predicted_label': ml_class[hasil],'detail' : predicted})
         else:
-                return jsonify({'error': 'tidak terdeteksi', })
+            return jsonify({'error': 'tidak terdeteksi'})
 
 
 @app.route('/logout', methods=['POST'])
 def logout():
     access_token = request.headers.get('Authorization')
-
     if not access_token:
         return jsonify({'error': 'Access token not found'}), 401
-
     try:
-        # Mendekode access token dan mendapatkan UID pengguna
         decoded_token = firebase.auth.verify_id_token(access_token)
         uid = decoded_token['uid']
-
-        # Menghapus refresh token pengguna dari Firebase Authentication
         firebase.auth.revoke_refresh_tokens(uid)
-
-        # Hapus access token dari sesi Flask
         session.clear()
-
         return jsonify({'message': 'Logout successful'})
-
     except Exception as e:
         print(str(e))
         return jsonify({'error': 'Failed to logout'}), 400
